f8f7d5df22dc16b95179bd343e4557d1f6360141
mobiuschair <mobiuschair001@gmail.com>
2025-01-31 17:18:47 +0000

    working code

---
+++ variables.jl
@@ -0,0 +1,25 @@
+x = 42
+pi = 3.14
+name = "Julia"
+
+print(12)
+print(12)
+
+println(12)
+println(12)
+
+name = "Alice"
+age = 30
+message = "Hi, I'm $name and I'm $age years old."
+println(message)
+
+calculation = "5 + 10 equals $(5+10)."
+println(calculation) 
+
+#This is a single-line comment
+x = 10
+
+#=
+This is a multi-line comment
+It can explain something in detail
+=#
